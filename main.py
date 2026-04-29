96fdbe96b5da2f143b5b3343eb540173cf987a4b
John Lorica <jroderick.0717@gmail.com>
2026-04-29 21:29:38 +0800

    A little more Kinter

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import tkinter as tk
 
+# To create the main window
 root = tk.Tk()
+# The title
 root.title('My First Project')
+# The size
 root.geometry('300x400')
 
+label = tk.Label(root, text='Hello, TKinter!')
+label.place(x=110, y=40)
+
+button = tk.Button(root, text='Click Me', command=lambda: label.config(
+    text='You clicked the button!'))
+button.place(x=110, y=80)
+
+# GUI event loop
 root.mainloop()
 
 # study TKinter Entry Widget and TKinter Button
