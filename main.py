6af1be7ad9b9f0e72ce1cf7840b8e0294f1f9c4a
John Lorica <jroderick.0717@gmail.com>
2026-05-20 23:00:34 +0800

    Figured out some classes and indentation stuff

--- main.py
+++ main.py
@@ -1,54 +1,60 @@
 import tkinter as tk
 from tkinter import messagebox
 
-# To create the main window
-root = tk.Tk()
-# The title
-root.title('My First Project')
-# The size
-root.geometry('300x400')
 
-label = tk.Label(root, text='Hello, TKinter!')
-label.place(x=110, y=40)
+class MyGUI:
 
-button = tk.Button(root, text='Start', command=lambda: label.config(
-    text='Welcome!'))
-button.place(x=120, y=80)
+    def __init__(self):
 
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
+        # To create the main window
+        root = tk.Tk()
+        # The title
+        root.title('My First Project')
+        # The size
+        root.geometry('300x400')
 
-btn1 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        label = tk.Label(root, text='Hello, TKinter!')
+        label.place(x=110, y=40)
 
-btn2 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        button = tk.Button(root, text='Start', command=lambda: label.config(
+            text='Welcome!'))
+        button.place(x=120, y=80)
 
-btn3 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        buttonframe = tk.Frame(root)
+        buttonframe.columnconfigure(0, weight=1)
+        buttonframe.columnconfigure(1, weight=1)
+        buttonframe.columnconfigure(2, weight=1)
 
-btn4 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn1 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn5 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn2 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn3 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn7 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn4 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn8 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn5 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn9 = tk.Button(buttonframe, text='1', font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn6 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-label = tk.Label(root, text='Placeholder')
-label.place(x=110, y=200)
+        btn7 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        btn8 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        btn9 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        label = tk.Label(root, text='Placeholder')
+        label.place(x=110, y=200)
+
 
 # GUI event loop
 root.mainloop()
