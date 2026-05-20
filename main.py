fc691e03cf76e3fc9f08c125616eaecd13d706ba
John Lorica <jroderick.0717@gmail.com>
2026-05-20 20:29:36 +0800

    Continue with Tkinter

--- main.py
+++ main.py
@@ -21,6 +21,31 @@ buttonframe.columnconfigure(1, weight=1)
 buttonframe.columnconfigure(2, weight=1)
 
 btn1 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn2 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn3 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn4 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn5 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn7 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn8 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn9 = tk.Button(buttonframe, text='1', font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
 label = tk.Label(root, text='Placeholder')
 label.place(x=110, y=200)
